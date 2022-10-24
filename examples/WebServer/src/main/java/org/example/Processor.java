25b5c14bb4c6d7c4d24cf0b61026432bd9d18c62
Aruzhan Muratbek <aruzhan_1106@mail.ru>
2022-10-24 15:48:02 +0600

    First commit

--- examples/WebServer/src/main/java/org/example/Processor.java
+++ examples/WebServer/src/main/java/org/example/Processor.java
@@ -16,22 +16,73 @@ public class Processor {
         this.request = request;
     }
 
-    public void process() throws IOException {
+    public void process() throws IOException, InterruptedException {
         System.out.println("Got request:");
         System.out.println(request.toString());
+        System.out.println(request.getRequestLine());
         System.out.flush();
 
         PrintWriter output = new PrintWriter(socket.getOutputStream());
 
-        // We are returning a simple web page now.
-        output.println("HTTP/1.1 200 OK");
-        output.println("Content-Type: text/html; charset=utf-8");
-        output.println();
-        output.println("<html>");
-        output.println("<head><title>Hello</title></head>");
-        output.println("<body><p>Hello, world!</p></body>");
-        output.println("</html>");
-        output.flush();
-        socket.close();
+        String[] splitRequest = request.getRequestLine().split(" ");
+
+        switch (splitRequest[1]) {
+            case "/create/itemid" -> {
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<html>");
+                output.println("<head><title>Create</title></head>");
+                output.println("<body><p>To create an item please specify the ID</p></body>");
+                output.println("</html>");
+                output.flush();
+                socket.close();
+            }
+            case "/delete/itemid" -> {
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<html>");
+                output.println("<head><title>Delete</title></head>");
+                output.println("<body><p>To delete an item please specify the ID</p></body>");
+                output.println("</html>");
+                output.flush();
+                socket.close();
+            }
+            case "/exec/params" -> {
+                // This will print 50 Fibonacci numbers
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<html>");
+                output.println("<head><title>hello</title></head>");
+                output.println("<body><p>");
+                int a = 0, b = 1, c;
+                for (int i = 2; i <= 40; i++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    output.print(b + " ");
+                }
+                output.println("</p></body>");
+                output.println("</html>");
+                output.flush();
+                Thread.sleep(1000);
+                socket.close();
+            }
+            default -> {
+                // We are returning a simple web page now.
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<html>");
+                output.println("<head><title>Hello</title></head>");
+                output.println("<body><p>Hello, world!</p></body>");
+                output.println("</html>");
+                output.flush();
+                socket.close();
+            }
+        }
     }
 }
--- examples/WebServer/src/main/java/org/example/WebServer.java
+++ examples/WebServer/src/main/java/org/example/WebServer.java
@@ -13,7 +13,7 @@ public class WebServer {
         // Port number for http request
         int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
         // The maximum queue length for incoming connection
-        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;;
+        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;
 
         try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
             System.out.println("Web Server is starting up, listening at port " + port + ".");
@@ -38,8 +38,9 @@ public class WebServer {
         }
         catch (IOException ex) {
             ex.printStackTrace();
-        }
-        finally {
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
             System.out.println("Server has been shutdown!");
         }
     }
